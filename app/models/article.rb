b39acb9d49b3b5e27fadf3d5c190bea330f07ac7
fmackojc <fab.mackojc@gmail.com>
2013-05-21 17:11:46 -0700

    remove Sinatra app and create rails skeleton

--- app/models/article.rb
+++
@@ -1,9 +0,0 @@
-class Article < ActiveRecord::Base
-  before_create :set_url
-
-  private
-  def set_url
-    time = Time.now
-    self.url = "articles/#{time.month}/#{time.day}/#{time.year}/#{self.title.downcase.gsub(' ', '-')}"
-  end
-end
---
+++ sweet_articles/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SweetArticles::Application.config.secret_token = 'e2341d63c40255571b8a28fb7efd538f653632bec5f3a0d86313352ca1639d30d28b050cf6c4a34f9341b006c9f95fba2d8d25f71c56d0aa50836efb2b0addb1'
